a1e01a1e7f0fc17ff32cbb7622705a2b7d49750b
zeze <89672780+mdmdr8@users.noreply.github.com>
2022-12-01 16:27:22 +0900

    주소 찾기 기능
    리액트 컴포넌트

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import SubwayApi from './SubwayApi';
 import RechartsLineTest from './RechartsLineTest';
-// import PostCodeApp from './PostCodeApp';
+import PostCodeApp from './PostCodeApp';
 function App() {
   return (
     <div className="App">
       <SubwayApi />
       {/* <RechartsLineTest/> */}
-      {/* <PostCodeApp/> */}
-      {/* <PostCodeApp /> */}
+      <PostCodeApp />
+
     </div>
   );
 }
